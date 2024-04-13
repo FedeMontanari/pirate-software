781514caafb773be5aebfe5c8e87117d3e05eef6
Federico Montanari <fedemontanari@outlook.com>
2024-04-13 09:01:32 -0300

    Fixed merge issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,12 @@ const pixel = Pixelify_Sans({
 export const metadata: Metadata = {
   title: "Pirate Software",
   description: "We make pretty rad games!",
+  openGraph: {
+    title: "Pirate Software",
+    description: "Pirate Software is a small team producing Indie Games!",
+    url: "https://pirate-software.vercel.app",
+    siteName: "Pirate Software",
+  },
 };
 
 export default function RootLayout({
